b7872251ec56626d8059ee7a2505afddcea73ae1
Linus Kim <101190589+theoryoflinus@users.noreply.github.com>
2025-07-27 13:34:19 +0900

    init

---
+++ scenes/component/health_component.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@export var max_health: float = 10 
+
+func damage(damage: float):
+	
---
+++ scenes/game_object/player/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 150.0
+const ACCELERATION_SMOOTHING = 25
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	var target_velocity = direction * MAX_SPEED
+	
+	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	move_and_slide()
+
+func get_movement_vector():
+	var movement_vector = Vector2.ZERO
+
+	# Use positive values first 
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
